a27fb0e494f9bb3a2adba9dc0fc7908e2a79f1c8
Javon Harper <javon.d.harper@gmail.com>
2018-02-21 15:46:38 -0500

    Initial commit

---
+++ rules.js
@@ -0,0 +1,15 @@
+const rules = {
+  'db': 'ahref=/cont/4/img.jpg',
+  'if': 'imgsrc=imf.to/images/thumb/ij/nm/img.jpg -> (thumb -> full)',
+  'imgyt': 'imgsrc=img.yt/upload/small/XXXX/XX/XX/XXXXXXXXXX.jpg -> (small -> big)',
+  'ph': [
+    'imgsrc',
+    'https://t17.pixhost.org/thumbs/88/XXXXX.jpg',
+    'https://img17.pixhost.org/images/88/XXXXXX.jpg',
+  ],
+  'acid': [
+    'imgsrc',
+    'https://acidimg.cc/upload/small/XX/XX/XX/XXXXXXXXXXX.jpg',
+    'https://i.acidimg.cc/big/XX/XX/XX/XXXXXXXXXXX.jpg'
+  ]
+}
