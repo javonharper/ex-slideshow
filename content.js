c6f948682992ad61e293690f12b9235340696987
Javon Harper <javon.d.harper@gmail.com>
2018-03-07 17:55:07 -0500

    Show preview at bottom of page

--- content.js
+++ content.js
@@ -1,45 +1,8 @@
 // @format
 
-console.log('hello from content.js')
-
 const { h, app } = hyperapp
 
-const fetchLinks = () => {
-  let links = []
-
-  // Gather links
-  $('a').each((i, el) => {
-    links.push(el.href)
-  })
-
-  $('img').each((i, el) => {
-    links.push(el.src)
-  })
-
-  return links
-}
-
-const extractUrls = links => {
-  const imageUrls = links.map(url => {
-    if (url.match(db.match)) {
-      return url.replace(db.match, db.replace)
-    }
-
-    if (url.match(yt.match)) {
-      return url.replace(yt.match, yt.replace)
-    }
-
-    if (url.match(ph.match)) {
-      return url.replace(ph.match, ph.replace)
-    }
-
-    return null
-  })
-
-  const images = imageUrls.filter(url => url !== null)
-
-  return images
-}
+console.log('hello from content.js')
 
 const state = {
   zoomed: true,
@@ -48,11 +11,13 @@ const state = {
 }
 
 const actions = {
+  close: () => state => $('#ex').remove(),
   toggleZoom: () => state => ({ zoomed: !state.zoomed }),
   back: () => state => ({ current: Math.max(state.current - 1, 0) }),
   next: () => state => ({
     current: Math.min(state.current + 1, state.images.length - 1),
   }),
+  setCurrent: i => state => ({ current: i }),
 }
 
 const view = (state, actions) =>
@@ -62,7 +27,7 @@ const view = (state, actions) =>
       h('span', { class: 'toolbar-item' }, 'All'),
       h('span', { class: 'toolbar-item', onclick: actions.back }, 'Back'),
       h('span', { class: 'toolbar-item', onclick: actions.next }, 'Next'),
-      h('span', { class: 'toolbar-item' }, 'Close'),
+      h('span', { class: 'toolbar-item', onclick: actions.close }, 'Close'),
     ]),
     h('div', { class: 'backdrop' }, [
       h('img', {
@@ -71,27 +36,85 @@ const view = (state, actions) =>
         src: state.images[state.current],
       }),
     ]),
+    h(
+      'div',
+      { class: 'thumbnails' },
+      state.images.map((image, i) =>
+        h('img', {
+          class: `thumbnail ${i === state.current ? 'active' : ''}`,
+          onclick: () => actions.setCurrent(i),
+          'data-src': image,
+        }),
+      ),
+    ),
   ])
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === 'clicked_browser_action') {
-    $(document.body).prepend('<div class="ex" />')
-
-    const links = fetchLinks()
-    const images = extractUrls(links)
-    const ex = app({ ...state, images }, actions, view, $('.ex')[0])
-
-    document.addEventListener('keydown', function(event) {
-      if (event.keyCode == 37) {
-        ex.back()
-      }
-
-      if (event.keyCode == 39) {
-        ex.next()
-      }
-    })
+    console.log('taking off...')
+    main()
   }
 })
+
+
+const main = () => {
+  $(document.body).prepend('<div id="ex" />')
+
+  const links = fetchLinks()
+  const images = extractUrls(links)
+  const ex = app({ ...state, images }, actions, view, $('#ex')[0])
+
+  document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 37) {
+      ex.back()
+    }
+
+    if (event.keyCode == 39) {
+      ex.next()
+    }
+  })
+
+
+  $(document).ready(() => $('img').unveil())
+}
+
+const fetchLinks = () => {
+  let links = []
+  $('a').each((i, el) => links.push(el.href))
+  $('img').each((i, el) => links.push(el.src))
+  return links
+}
+
+const extractUrls = links => {
+  const images = links
+    .map(url => {
+      if (url.match(db.match)) {
+        return url.replace(db.match, db.replace)
+      }
+
+      if (url.match(yt.match)) {
+        return url.replace(yt.match, yt.replace)
+      }
+
+      if (url.match(ph.match)) {
+        return url.replace(ph.match, ph.replace)
+      }
+
+      if (url.match(ig.match)) {
+        return url.replace(ig.match, ig.replace)
+      }
+
+      return null
+    })
+    .filter(url => url !== null)
+
+  return images
+}
+
+const ig = {
+  match: /(.+)\.jpg/,
+  replace: '$1.jpg',
+}
 
 const db = {
   match: /(.+)_t\.jpg/,
